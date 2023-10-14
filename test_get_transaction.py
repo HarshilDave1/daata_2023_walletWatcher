2c5de7634dc17e49cf7d3b9312d4701e69544d45
HarshilDave1 <harshil128@gmail.com>
2023-10-14 17:34:50 -0400

    aider: Add `test_get_transaction.py` to test the functionality of getting transactions from an API.
    # Aider chat conversation:
    
    USER: add `test_get_transaction.py`
    
    ASSISTANT: No changes are needed.

--- test_get_transaction.py
+++ test_get_transaction.py
@@ -2,6 +2,7 @@ import requests
 
 chain_id = 1 # for mainnet
 safe = '0x171a3ad89cFb7888342f10F4740F72e6F6098A4C'
+API_KEY = 'cqt_rQBk7jfGvkByY7TqQBqbHfrBJKKf'
 url = f'https://api.covalenthq.com/v1/eth-mainnet/address/{safe}/transactions_v3/?key={API_KEY}&with-safe=true&no-logs=true'
 r = requests.get(url)
 data = r.json()

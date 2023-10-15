762f2b93285b7c0f932bd2ebb971bea85a56b2e3
HarshilDave1 <harshil128@gmail.com>
2023-10-15 01:04:41 -0400

    make database functions

---
+++ database.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+def setup_database():
+    conn = sqlite3.connect('transactions.db')
+    cursor = conn.cursor()
+    
+    # Create a table to store transactions
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS transactions (
+        tx_hash TEXT PRIMARY KEY,
+        to_address TEXT,
+        value REAL,
+        gas_spent INTEGER,
+        fees_paid REAL
+    )
+    ''')
+    conn.commit()
+    conn.close()
+
+def insert_transaction(tx):
+    conn = sqlite3.connect('transactions.db')
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+    INSERT OR IGNORE INTO transactions (tx_hash, to_address, value, gas_spent, fees_paid)
+    VALUES (?, ?, ?, ?, ?)
+    ''', (tx['tx_hash'], tx['to_address'], float(tx['value_quote']), tx['gas_spent'], float(tx['fees_paid'])))
+    
+    conn.commit()
+    conn.close()
+
+def fetch_and_store_transactions():
+    transactions = fetch_transactions()
+    for tx in transactions['data']['items']:
+        insert_transaction(tx)
+
+def transaction_exists(tx_hash):
+    conn = sqlite3.connect('transactions.db')
+    cursor = conn.cursor()
+    
+    cursor.execute('SELECT 1 FROM transactions WHERE tx_hash = ?', (tx_hash,))
+    exists = cursor.fetchone()
+    
+    conn.close()
+    return exists is not None
+
+
+if __name__ == "__main__":
+    setup_database()

24f25c52e516822d30d6529d5821b37c76f22691
HarshilDave1 <harshil128@gmail.com>
2023-10-15 13:45:44 -0400

    update database to include blocktime

--- database.py
+++ database.py
@@ -12,7 +12,8 @@ def setup_database():
         to_address TEXT,
         value REAL,
         gas_spent INTEGER,
-        fees_paid REAL
+        fees_paid REAL,
+        block_signed_at TEXT
     )
     ''')
     conn.commit()
@@ -23,9 +24,9 @@ def insert_transaction(tx):
     cursor = conn.cursor()
     
     cursor.execute('''
-    INSERT OR IGNORE INTO transactions (tx_hash, from_address, to_address, value, gas_spent, fees_paid)
-    VALUES (?, ?, ?, ?, ?,?)
-    ''', (tx['tx_hash'], tx['from_address'], tx['to_address'], float(tx['value']), tx['gas_spent'], float(tx['fees_paid'])))
+    INSERT OR IGNORE INTO transactions (tx_hash, from_address, to_address, value, gas_spent, fees_paid, block_signed_at)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    ''', (tx['tx_hash'], tx['from_address'], tx['to_address'], float(tx['value']), tx['gas_spent'], float(tx['fees_paid']), tx['block_signed_at']))
     
     conn.commit()
     conn.close()

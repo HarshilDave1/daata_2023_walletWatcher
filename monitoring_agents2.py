2b7c5958c430258870acb41701e1e2b293908119
HarshilDave1 <harshil128@gmail.com>
2023-10-15 16:20:54 -0400

    Agents working right

--- monitoring_agents2.py
+++ monitoring_agents2.py
@@ -1,6 +1,7 @@
 import autogen
 import os
 from dotenv import load_dotenv
+from analyzerAgent import AnalyzerAgent
 
 load_dotenv()
 
@@ -21,12 +22,12 @@ class AnomalyDetectionAgent:
             llm_config=config_list,
             system_message="Monitors the user's wallet for any transactions. Reports transactions in real-time. Respond 'Terminate' if no task given. "
         )
-        
+                
         # Analyzer Agent
         self.analyzer = autogen.AssistantAgent(
             name="Analyzer",
             llm_config=config_list,
-            system_message="Analyzes transactions for potential risks. Uses AI and predefined rules to assess transaction safety."
+            system_message="Analyzes transactions for potential risks. Uses AI and predefined rules to assess transaction safety. Respond 'Terminate' if no task given."
         )
         
         # Notifier Agent
@@ -45,7 +46,8 @@ class AnomalyDetectionAgent:
         
         # User Proxy Agent
         self.user_proxy = autogen.UserProxyAgent(
-            name="User Proxy",
+            name="User_Proxy",
+            code_execution_config={"last_n_messages": 3, "work_dir": "groupchat"},
             system_message="Represents the user. Receives alerts and provides feedback or directives to other agents. TERMINATE if task is completed.",
             human_input_mode="ALWAYS",
             is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
@@ -76,14 +78,6 @@ class AnomalyDetectionAgent:
         self.groupchat = autogen.GroupChat(
             agents=[self.monitor, self.analyzer, self.notifier, self.guardian, self.user_proxy, self.auditor, self.planner, self.critic],
             messages=[
-                        "Monitor: Please provide details of the latest transactions for the user's wallet. Respond 'Terminate' if no task given ",
-                        "Analyzer: Assess these transactions for potential risks using AI models and predefined rules.",
-                        "Notifier: If any high-risk transactions are detected, alert the User Proxy with details.",
-                        "Guardian: Based on the risk assessment, suggest protective actions if necessary.",
-                        "User Proxy: Please review any alerts and provide feedback or directives.",
-                        "Auditor: Periodically review the actions taken and provide feedback.",
-                        "Planner: Refine the strategy based on feedback from the Auditor and User Proxy.",
-                        "Critic: Ensure all agents are functioning correctly and provide feedback where necessary.",
                         ],
             max_round=5
         )
@@ -91,20 +85,28 @@ class AnomalyDetectionAgent:
         # Manager for the group chat
         self.manager = autogen.GroupChatManager(groupchat=self.groupchat, llm_config=config_list)
 
-    def detect_anomalies(self, transactions):
+    def detect_anomalies(self):
+        analyzer = AnalyzerAgent()
+        analyzer.load_model()
+        anomalies = analyzer.predict()
+        formatted_anomalies = "\n\n".join([str(anomaly) for anomaly in anomalies])
+
         message = f"""
-        Current Transaction Details: {transactions}
+        Detected Anomalies:
+        {formatted_anomalies}
+        
+        Please check the importance of each anomaly. Transactions with an anomaly score close to -1 are considered highly anomalous and may require immediate attention.
         """
         response = self.initiate_chat(message)
         return response
 
     def initiate_chat(self, message):
-        self.user_proxy.initiate_chat(self.monitor, message=message)
+        self.user_proxy.initiate_chat(self.manager, message=message)
         return autogen.ChatCompletion.logged_history
 
 
 initial_prompt = """
-
+Say Hello
 """
 
 def main():
